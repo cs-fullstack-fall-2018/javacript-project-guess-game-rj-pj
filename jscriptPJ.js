da20705dad655a852cef348eda5f0557ca247d35
Pelane Sanon <p_sanon@aol.com>
2018-08-24 11:02:35 -0500

    New imgs

---
+++ jscriptPJ.js
@@ -0,0 +1,21 @@
+// An array of selector images
+
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = 'planet0.png';
+
+imgArray[1] = new Image();
+imgArray[1].src = 'planet1.png';
+
+imgArray[2] = new Image();
+imgArray[2].src = 'planet2.png';
+
+imgArray[3] = new Image();
+imgArray[3].src = 'planet3.png';
+
+imgArray[4] = new Image();
+imgArray[4].src = 'planet4.png';
+
+imgArray[5] = new Image();
+imgArray[5].src = 'planet5.png';
